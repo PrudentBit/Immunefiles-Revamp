921eb66251ca7c6c43a67c1be330de0b03a04dbc
KilloWatts <keshav2552003@gmail.com>
2023-12-06 18:48:45 +0530

    User Profile Card complete

--- src/components/UserProfileCard.tsx
+++ src/components/UserProfileCard.tsx
@@ -4,6 +4,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import Image from "next/image"
+import { Button } from "./ui/button"
 
 type Props = {
   userDetails?: UserDetailsType
@@ -12,7 +13,7 @@ type Props = {
 const UserProfileCard = ({userDetails}: Props) => {
   return (
     <Popover>
-      <PopoverTrigger>
+      <PopoverTrigger >
         <div className='flex gap-2 cursor-pointer'>
           <Image src="/user.svg" alt='profile' width={45} height={45} className='rounded-lg'/>
           <div className='text-left'>
@@ -21,12 +22,43 @@ const UserProfileCard = ({userDetails}: Props) => {
           </div>
         </div>
       </PopoverTrigger>
-      <PopoverContent>
-        <div className='flex gap-2 cursor-pointer'>
-          <Image src="/user.svg" alt='profile' width={45} height={45} className='rounded-lg'/>
-          <div className='text-left'>
-            <p className='text-black font-normal text-sm h-4'>Hello</p>
-            <p className='text-primary_font_2 text-lg font-semibold whitespace-nowrap'>{userDetails?.name}</p>
+      <PopoverContent className="mr-4 rounded-2xl w-[18.5rem] flex flex-col gap-5 p-5">
+        <div className='flex gap-3 cursor-pointer w-full'>
+          <div className="min-w-[6rem]">
+            <Image src="/user.svg" alt='profile' width={90} height={90} className='rounded-lg'/>
+          </div>
+          <div className='text-left h-[5.3rem] w-[9rem] flex flex-col justify-between'>
+            <p className='text-lg font-semibold whitespace-nowrap leading-6 truncate'>{userDetails?.name}</p>
+            <p className='text-gray-400 text-sm whitespace-nowrap'>@{userDetails?.username}</p>
+            <Button className="text-primary_font h-8 w-full px-2 mt-2 text-sm font-medium leading-4 bg-white border-solid border-[1px] border-primary_font hover:bg-primary_font hover:text-white rounded-lg">
+              Manage Groups
+            </Button>
+          </div>
+        </div>
+        <div className="flex gap-2 flex-wrap w-fit">
+          <div className="pl-2 pr-4 h-7 w-max rounded-full flex items-center  gap-2 bg-[#E5EDFF]">
+            <Image src="/suitcase-icon.svg" alt='company' width={20} height={20}/>
+            <p className='text-primary_font text-sm font-normal leading-4'>{userDetails?.domain}</p>
+          </div>
+
+          <div className="pl-2 pr-4 h-7 rounded-full flex items-center  gap-2 bg-bg_hover">
+            <Image src="/employee-icon.svg" alt='company' width={20} height={20}/>
+            <p className='text-primary_font_2 text-sm font-normal leading-4'>Employee</p>
+          </div>
+
+          <div className="pl-2 pr-4 h-7 rounded-full flex items-center  gap-2 bg-[#E5EDFF]">
+            <Image src="/mail-icon.svg" alt='company' width={20} height={20}/>
+            <p className='text-primary_font text-sm font-normal leading-4'>{userDetails?.email}</p>
+          </div>
+
+          <div className="pl-2 pr-4 h-7 rounded-full flex items-center  gap-2 bg-bg_hover">
+            <Image src="/phone-icon.svg" alt='company' width={16} height={16}/>
+            <p className='text-primary_font_2 text-sm font-normal leading-4'>{userDetails?.phone_number}</p>
+          </div>
+
+          <div className="pl-2 pr-4 h-7 rounded-full flex items-center  gap-2 bg-[#D0FFE3]">
+            <Image src="/admin-icon-3.svg" alt='company' width={20} height={20}/>
+            <p className='text-[#3ABA6E] text-sm font-normal leading-4'>Admin</p>
           </div>
         </div>
       </PopoverContent>
