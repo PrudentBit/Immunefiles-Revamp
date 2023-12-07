d1e96514e8a4d28c7cbb0a5ebc4b91dc217dfb83
KilloWatts <keshav2552003@gmail.com>
2023-12-07 20:51:52 +0530

    Added group categories

--- src/components/Groups/GroupsTab.tsx
+++ src/components/Groups/GroupsTab.tsx
@@ -28,13 +28,19 @@ const GroupsTab = (props: Props) => {
 
   return (
     <div className='h-full w-full flex flex-col gap-7'>
-      <div className='flex gap-4 pt-2'>
-        <button title='Share' className='rounded-full h-10 w-10 flex items-center justify-center p-2 bg-[#E5EDFF]  hover:bg-bg_hover'>
-          <Image src='/new-groups-icon.svg' width={24} height={24} alt='Link icon'/>
-        </button>
-        <button title='Delete' className='rounded-full h-10 w-10 flex items-center justify-center p-2 bg-[#FFE3E5]  hover:bg-[#FFCDD0]'>
-          <DeleteGroupAlert/>
-        </button>
+      <div className='flex justify-between items-center'>
+        <div className='flex gap-4 pt-2'>
+          <button title='Share' className='rounded-full h-10 w-10 flex items-center justify-center p-2 bg-[#E5EDFF]  hover:bg-bg_hover'>
+            <Image src='/new-groups-icon.svg' width={24} height={24} alt='Link icon'/>
+          </button>
+          <button title='Delete' className='rounded-full h-10 w-10 flex items-center justify-center p-2 bg-[#FFE3E5]  hover:bg-[#FFCDD0]'>
+            <DeleteGroupAlert/>
+          </button>
+        </div>
+
+        <div className='flex gap-4 pt-2'>
+          
+        </div>
       </div>
 
       <div className='flex flex-col gap-4'>
@@ -46,11 +52,30 @@ const GroupsTab = (props: Props) => {
             alt="groups"
             className="ml-[2px]"
           />
-          <p className="text-primary_font font-semibold text-xl pb-[0.1rem]">My Groups</p>
+          <p className="text-primary_font font-semibold text-xl pb-[0.1rem]">As admin</p>
         </div>
 
         <div className='flex flex-wrap gap-4 p-1'>
-          {groups?.map(group => (
+          {groups?.filter(group => group.admin).map(group => (
+            <Groups key={group.urlhash} group={group}/>
+          ))}
+        </div>
+      </div>
+
+      <div className='flex flex-col gap-4'>
+        <div className="flex gap-2">
+          <Image
+            src="/admin-icon-2.svg"
+            width={24}
+            height={24}
+            alt="groups"
+            className="ml-[2px]"
+          />
+          <p className="text-primary_font font-semibold text-xl pb-[0.1rem]">As member</p>
+        </div>
+
+        <div className='flex flex-wrap gap-4 p-1'>
+          {groups?.filter(group => !group.admin).map(group => (
             <Groups key={group.urlhash} group={group}/>
           ))}
         </div>
